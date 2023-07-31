9d377d47f9a2b3c6762a5fcea7472e89981f5adf
dreinicke27 <danicareinicke@gmail.com>
2023-07-31 12:14:14 -0700

    commenting out all dynamo stuff for now

--- app/routes/cart.py
+++ app/routes/cart.py
@@ -1,14 +1,20 @@
 from flask import Blueprint, request, redirect, jsonify
 import stripe
-import controller as dynamodb
+import boto3
 
 cartBP = Blueprint("cart", __name__, url_prefix="")
 
 @cartBP.route("/create-checkout-session", methods=['POST'])
 def create_checkout_session():
-    # data = request.json
+    data = request.json
     # #try to get line items from request rather than here...have to be in the body from the form 
-    # line_item_data = data['line_items']
+    line_item_data = data['line_items']
+    # if price == 165, price_key = the one here, else, the other one
+    # for loop for each item in line item data, return {
+    #    'price': 'price_1NYBhpFzSGyLRwvaltRdFK2s',
+    #               'quantity': 1,
+    # }
+
     try:
         checkout_session = stripe.checkout.Session.create(
             line_items=[
@@ -28,23 +34,23 @@ def create_checkout_session():
 
     return redirect(checkout_session.url, code=303)
 
-@cartBP.route("/")
-def root():
-    dynamodb.create_table_cart()
-    return 'Table created'
+# @cartBP.route("/")
+# def root():
+#     dynamodb.create_table_cart()
+#     return 'Table created'
 
-@cartBP.route("/cart/<id>", methods=["POST"])
-def create_cart():
-    data = request.get_json()
-    response = dynamodb.write_to_cart_table(data['id'], data['products'])    
-    if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
-        return {
-            'msg': 'Added successfully',
-        }
-    return {  
-        'msg': 'Some error occcured',
-        'response': response
-    }
+# @cartBP.route("/cart/<id>", methods=["POST"])
+# def create_cart():
+#     data = request.get_json()
+#     response = dynamodb.write_to_cart_table(data['id'], data['products'])    
+#     if (response['ResponseMetadata']['HTTPStatusCode'] == 200):
+#         return {
+#             'msg': 'Added successfully',
+#         }
+#     return {  
+#         'msg': 'Some error occcured',
+#         'response': response
+#     }
 
 # @cartBP.route("/cart/<id>", methods=["GET"])
 # def get_cart(id):
